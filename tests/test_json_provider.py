341e6d3c7d7a983a13fc6378e805fa87b6a19fce
crood58 <crood58@gmail.com>
2023-04-10 23:31:21 +0000

    Completed JSON tests.

--- tests/test_json_provider.py
+++ tests/test_json_provider.py
@@ -5,9 +5,12 @@ import json
 import pytest
 
 from bson import ObjectId
-from quart import Quart, jsonify
-from quart_mongo import Mongo, MongoJSONProvider
+from quart import Quart, jsonify, request, Response
+from quart.typing import TestClientProtocol
 from six import ensure_str
+from quart_mongo import Mongo, MongoJSONProvider
+
+from .utils import teardown
 
 @pytest.mark.asyncio
 async def test_provider_registered_with_app(
@@ -31,13 +34,63 @@ async def test_encodes_json(app: Quart) -> None:
         assert dumped == {"foo": "bar"}
 
 @pytest.mark.asyncio
-async def test_handles_mongo_types(app: Quart, client_uri: str) -> None:
+async def test_handles_mongo_types(
+    app: Quart, uri: str, client: TestClientProtocol
+    ) -> None:
     """
     Tests that JSON encoding handles Mongo types.
     """
-    Mongo(app, client_uri)
-    async with app.app_context():
-        resp = jsonify({"id": ObjectId("5cf29abb5167a14c9e6e12c4")})
-        dumped = app.json.loads(ensure_str(await resp.get_data()))
-        assert dumped == {"id": {"$oid": "5cf29abb5167a14c9e6e12c4"}}
+    Mongo(app, uri)
+    id = "5cf29abb5167a14c9e6e12c4"
 
+    @app.route("/get", methods=["GET"])
+    async def get() -> Response:
+        """
+        Returns a JSON response for testing.
+        """
+        return jsonify({"id": ObjectId(id)})
+
+    @app.route("/post", methods=["POST"])
+    async def post():
+        """
+        Gets JSON from a request object.
+        """
+        data = await request.json()
+        assert data == {"id": {"$oid": id}}
+
+    response = await client.get("/get")
+    data = await response.get_json()
+    await client.post("/post", json=data)
+
+@pytest.mark.asyncio
+async def test_jsonifies_cursor(
+    app: Quart, mongo_uri: Mongo, client: TestClientProtocol
+    ) -> None:
+    """
+    Test JSON encoding for Mongo cursor.
+    """
+    mongo = mongo_uri
+
+    @app.route("/get", methods=["GET"])
+    async def get() -> Response:
+        """
+        Returns a JSON response for testing.
+        """
+        await mongo.db.rows.insert_many([{"foo": "bar"}, {"foo": "baz"}])
+        curs = await mongo.db.row.find(projection={"_id": False})
+        curs = curs.sort("foo")
+        return jsonify(curs)
+
+    @app.route("/post", methods=["POST"])
+    async def post():
+        """
+        Gets JSON from a request object.
+        """
+        data = await request.json()
+        assert data == [{"foo": "bar"}, {"foo": "baz"}]
+
+    await app.startup()
+    response = await client.get("/get")
+    data = await response.get_json()
+    await client.post("/post", json=data)
+    await teardown(mongo)
