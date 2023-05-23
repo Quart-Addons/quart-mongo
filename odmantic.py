eeb88e4f59dda4f81914c547072d8e0ab85ffa38
crood58 <crood58@gmail.com>
2023-05-23 00:57:56 +0000

    quick test.

---
+++ odmantic.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from uuid import UUID
+from odmantic import Model
+
+class OdmanticEncoded(Model):
+    a: UUID
+    b: Path
+
+odm = OdmanticEncoded(a=UUID("23ef2e02-1c20-49de-b05e-e9fe2431c474"), b=Path("/"))
+
+print(odm.doc())
