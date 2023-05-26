3c9e06ed68c50ddf9a168c4d27579f9bc2c9cc4f
crood58 <crood58@gmail.com>
2023-05-26 15:52:55 +0000

    Fixed version.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Quart-Mongo'
 copyright = '2023, Chris Rood'
 author = 'Chris Rood'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
